6dd3c112858f1b07d9a0c79dff8960f1568744b0
David L. Wire <dlwire@gmail.com>
2013-03-23 16:53:16 -0500

    Finished first iteration, limit applies to all branches. Command line output enabled

--- changesets-per-push-hook/features/steps.py
+++ changesets-per-push-hook/features/steps.py
@@ -6,6 +6,7 @@ from shutil import rmtree
 from subprocess import call
 from subprocess import Popen
 from subprocess import PIPE
+import os
 
 @before.each_scenario
 def setup(scenario):
@@ -30,7 +31,7 @@ def given_a_web_served_repository(step):
         f.write('allow_push=*\n')
         f.write('push_ssl=false\n')
 
-    world.process = Popen(['hg', 'serve', '-a', 'localhost'], cwd='web-served-repo', stdout=PIPE, stderr=PIPE)
+    world.process = Popen(['hg', 'serve', '-a', 'localhost'], cwd='web-served-repo')#, stdout=PIPE, stderr=PIPE)
 
     assert world.process is not None, "Unable to start webserver"
 
@@ -43,31 +44,35 @@ def and_the_changset_limiting_hook(step):
 
 @step(u'a local clone')
 def and_a_local_clone(step):
-    call(['hg', 'clone', 'http://localhost:8000', 'local-repo'], stdout=PIPE)
+    while 0 is not call(['hg', 'clone', 'http://localhost:8000', 'local-repo']):#, stdout=PIPE):
+        pass
 
     assert path.isdir('local-repo/.hg'), "Unable to clone webserver"
 
 @step(u'I set the changesets per push limit to (\d)')
 def when_i_set_changesets_per_push_limit(step, changeset_limit):
-    pass
+    with open('web-served-repo/.hg/.limit-changesets-per-push.config', 'w') as f:
+        f.write(changeset_limit)
+
+    assert path.isfile('web-served-repo/.hg/.limit-changesets-per-push.config'), os.getcwd()
     
 @step(u'I try to push (\d) changesets to the web-served repository')
 def and_i_try_to_push_to_the_web_served_repository(step, changeset_count):
-    for i in range(1, int(changeset_count)+2):
-        call(['touch', 'temp %d' % i], cwd='local-repo', stdout=PIPE)
+    for i in range(1, int(changeset_count)+1):
+        call(['touch', 'temp %d' % i], cwd='local-repo')#, stdout=PIPE)
         world.files_in_push.append('temp %d' % i)
-        call(['hg', 'add', 'temp %d' % i], cwd='local-repo', stdout=PIPE)
-        call(['hg', 'commit', '-m"message %d"' % i, '-u"user"'], cwd='local-repo', stdout=PIPE)
+        call(['hg', 'add', 'temp %d' % i], cwd='local-repo')#, stdout=PIPE)
+        call(['hg', 'commit', '-m"message %d"' % i, '-u"user"'], cwd='local-repo')#, stdout=PIPE)
 
-    call(['hg', 'push'], cwd='local-repo', stdout=PIPE)
+    call(['hg', 'push'], cwd='local-repo')#, stdout=PIPE)
    
 @step(u'my changesets are not accepted')
 def then_my_changesets_are_not_accepted(step):
-    call(['hg', 'update'], cwd='web-served-repo', stdout=PIPE)
+    call(['hg', 'update'], cwd='web-served-repo')#, stdout=PIPE)
     assert not any(path.isfile('web-served-repo/' + filename) for filename in world.files_in_push), "Some files were present"
    
 @step(u'my changesets are accepted')
 def then_my_changesets_are_accepted(step):
-    call(['hg', 'update'], cwd='web-served-repo', stdout=PIPE)
+    call(['hg', 'update'], cwd='web-served-repo')#, stdout=PIPE)
     assert all(path.isfile('web-served-repo/' + filename) for filename in world.files_in_push), "Some files were missing"
 
--- changesets-per-push-hook/limit-changesets-per-push.py
+++ changesets-per-push-hook/limit-changesets-per-push.py
@@ -1,7 +1,28 @@
 import sys
 sys.path.append("/Library/Python/2.7/site-packages/")
 
-from mercurial import util as hg
+from mercurial import util
+
+def get_incoming_changesets(repo, node):
+    """ Method to get the changesets in the repo coming in this push, stolen from the webs"""
+    return [repo[rev] for rev in xrange(repo[node].rev(), len(repo))]
+
+def get_changeset_limit(repo_root_dir):
+    with open(repo_root_dir + '/.hg/.limit-changesets-per-push.config') as f:
+        return int(f.read())
+    return 0
+
+def is_set_to_no_limit(changeset_limit):
+    return changeset_limit is 0
 
 def check_changeset_limit(ui, repo, node=None, **kwargs):
-    print('Running changeset limit hook. Limit is set to 2')
+    print('Running changeset limit hook')
+    
+    changeset_limit = get_changeset_limit(repo.root)
+    if is_set_to_no_limit(changeset_limit):
+        return;
+
+    incoming_changesets = len(get_incoming_changesets(repo, node))
+    if changeset_limit < incoming_changesets:
+        raise util.Abort('You tried to push %d changesets, the limit for this repository is %d! Please compress your changes and push again' % (incoming_changesets, changeset_limit))
+
