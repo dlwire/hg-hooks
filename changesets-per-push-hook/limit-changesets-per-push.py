8e95f948f0bf3ac6019afa05a40a004e17af7cc9
David L. Wire <dlwire@gmail.com>
2013-03-25 08:17:29 -0500

    extract out repository object

--- changesets-per-push-hook/limit-changesets-per-push.py
+++ changesets-per-push-hook/limit-changesets-per-push.py
@@ -1,12 +1,19 @@
 from mercurial import util
 
-def get_incoming_changesets(repo, node):
-    """ Method to get the changesets in the repo coming in this push, stolen from the webs"""
-    return [repo[rev] for rev in xrange(repo[node].rev(), len(repo))]
+class Repository(object):
+    def __init__(self, repo, node):
+        self.repo = repo
+        self.node = node
 
-def get_number_of_changesets_on_default(changesets):
-    return len([changeset for changeset in changesets if changeset.branch() is 'default'])
+    def get_changesets_on_branch(self, branch):
+        return [changeset for changeset in self.get_changesets() if changeset.branch() is branch]
 
+    def get_changesets(self):
+        return [self.repo[rev] for rev in xrange(self.repo[self.node].rev(), len(self.repo))]
+
+    def get_root_directory(self):
+        return self.repo.root
+#
 def get_changeset_limit(repo_root_dir):
     with open(repo_root_dir + '/.hg/.limit-changesets-per-push.config') as f:
         return int(f.read())
@@ -17,12 +24,15 @@ def is_set_to_no_limit(changeset_limit):
 
 def check_changeset_limit(ui, repo, node=None, **kwargs):
     print('Running changeset limit hook')
+
+    hg = Repository(repo, node)
     
-    changeset_limit = get_changeset_limit(repo.root)
+    changeset_limit = get_changeset_limit(hg.get_root_directory())
     if is_set_to_no_limit(changeset_limit):
         return;
 
-    incoming_changesets = get_number_of_changesets_on_default(get_incoming_changesets(repo, node))
+    incoming_changesets = len(hg.get_changesets_on_branch('default'))
     if changeset_limit < incoming_changesets:
         raise util.Abort('You tried to push %d changesets, the limit for this repository is %d! Please compress your changes and push again' % (incoming_changesets, changeset_limit))
+
 
